f2f75c40d538fef059d28effaed7036248232cf1
Timur <t.mugalimov@yandex.ru>
2023-06-21 23:09:54 +0500

    Фиксация проекта 21.06/

--- src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
@@ -7,7 +7,4 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByItem_Id(Long itemId);
-
-    List<Comment> findAllByItemOrderById(Item item);
 }
