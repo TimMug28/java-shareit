152938da1998671f37c01003e05db1bc1e5fef61
Timur <t.mugalimov@yandex.ru>
2023-06-21 23:10:09 +0500

    Фиксация проекта 21.06/

--- src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/comment/repository/CommentRepository.java
@@ -2,9 +2,6 @@ package ru.practicum.shareit.item.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.comment.Comment;
-import ru.practicum.shareit.item.model.Item;
-
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
